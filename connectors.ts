4137b4fa470e95d9a26974adc043b19c89e5359d
Rian Khanafi <51011550+RianKhanafi@users.noreply.github.com>
2022-01-28 00:04:27 +0700

    Initial commit

---
+++ connectors.ts
@@ -0,0 +1,76 @@
+import { InjectedConnector } from '@web3-react/injected-connector'
+import { NetworkConnector } from '@web3-react/network-connector'
+import { WalletConnectConnector } from '@web3-react/walletconnect-connector'
+import { WalletLinkConnector } from '@web3-react/walletlink-connector'
+import { LedgerConnector } from '@web3-react/ledger-connector'
+import { TrezorConnector } from '@web3-react/trezor-connector'
+import { LatticeConnector } from '@web3-react/lattice-connector'
+import { FrameConnector } from '@web3-react/frame-connector'
+import { AuthereumConnector } from '@web3-react/authereum-connector'
+import { FortmaticConnector } from '@web3-react/fortmatic-connector'
+import { MagicConnector } from '@web3-react/magic-connector'
+import { PortisConnector } from '@web3-react/portis-connector'
+import { TorusConnector } from '@web3-react/torus-connector'
+import { BitskiConnector } from './bitski-connector'
+
+const POLLING_INTERVAL = 12000
+const RPC_URLS: { [chainId: number]: string } = {
+  137: 'https://api.bitski.com/v1/web3/polygon',
+  80001: 'https://api.bitski.com/v1/web3/mumbai'
+}
+
+export const injected = new InjectedConnector({ supportedChainIds: [1, 3, 4, 5, 42] })
+
+export const network = new NetworkConnector({
+  urls: { 137: RPC_URLS[137], 80001: RPC_URLS[80001] },
+  defaultChainId: 137
+})
+
+export const walletconnect = new WalletConnectConnector({
+  rpc: { 1: RPC_URLS[1] },
+  qrcode: true
+})
+
+export const walletlink = new WalletLinkConnector({
+  url: RPC_URLS[1],
+  appName: 'web3-react example',
+  supportedChainIds: [1, 3, 4, 5, 42, 10, 137, 69, 420, 80001]
+})
+
+export const ledger = new LedgerConnector({ chainId: 1, url: RPC_URLS[1], pollingInterval: POLLING_INTERVAL })
+
+export const trezor = new TrezorConnector({
+  chainId: 1,
+  url: RPC_URLS[1],
+  pollingInterval: POLLING_INTERVAL,
+  manifestEmail: 'dummy@abc.xyz',
+  manifestAppUrl: 'http://localhost:1234'
+})
+
+export const lattice = new LatticeConnector({
+  chainId: 4,
+  appName: 'web3-react',
+  url: RPC_URLS[4]
+})
+
+export const frame = new FrameConnector({ supportedChainIds: [1] })
+
+export const authereum = new AuthereumConnector({ chainId: 42 })
+
+export const fortmatic = new FortmaticConnector({ apiKey: process.env.FORTMATIC_API_KEY as string, chainId: 4 })
+
+export const bitski = new BitskiConnector({
+  clientId: '0c100b9a-168a-4ed6-8eee-e11857e188f2',
+  chainId: 137,
+  callbackUrl: 'http://localhost:3000/public/callback.html'
+})
+
+export const magic = new MagicConnector({
+  apiKey: process.env.MAGIC_API_KEY as string,
+  chainId: 4,
+  email: 'hello@example.org'
+})
+
+export const portis = new PortisConnector({ dAppId: process.env.PORTIS_DAPP_ID as string, networks: [1, 100] })
+
+export const torus = new TorusConnector({ chainId: 1 })
